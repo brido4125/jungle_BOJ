cbe82d743e1f9154eed56ace80519f0b9ac98d45
hcs4125 <hcs4125@gmail.com>
2022-09-25 22:05:44 +0900

    update:2798

---
+++ changsub/2798.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+
+nums = list(map(int, input().split()))
+
+max = 0
+
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            sum = nums[i] + nums[j] + nums[k]
+            if max < sum <= M:
+                max = sum
+
+
+print(max)
