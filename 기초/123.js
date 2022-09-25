25ce41bb2f7c88f3ca256c922f647b68b4419690
Heesan Kim <opqryt45@gmail.com>
2022-09-25 09:30:55 +0900

    update:0925

---
+++ 기초/123.js
@@ -0,0 +1,14 @@
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+// for (let i = 0; i < arr2.length; i++) {
+//   result = arr1.push(arr2[i]);
+  
+// }
+
+// console.log(result);
+// console.log(arr1);
+
+arr1.push(arr2)
+
+console.log(arr1)
