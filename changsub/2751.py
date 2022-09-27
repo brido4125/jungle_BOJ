60dfcf3b170499787e2f5329c940924f4d21985b
hcs4125 <hcs4125@gmail.com>
2022-09-27 14:32:11 +0900

    update:2751

--- changsub/2751.py
+++ changsub/2751.py
@@ -1,5 +1,43 @@
+import sys
 
-# N이 1,000,000이 최대값
-# 제한 시간 2초 => 2억번
-# O(n*logN) = 6 * 1,000,000 => 6백만번 이라서 가능
-# n * logN 가능 => 힙 or 병합 정렬 사용
+N = int(input())
+nums = []
+for _ in range(N):
+    nums.append(int(sys.stdin.readline()))
+
+
+def merge(arr, tmp, start, mid, end):
+    for i in range(start, end + 1):
+        tmp[i] = arr[i]
+    part1 = start
+    part2 = mid + 1
+    index = start
+    while part1 <= mid and part2 <= end:
+        if tmp[part1] <= tmp[part2]:
+            arr[index] = tmp[part1]
+            part1 += 1
+        else:
+            arr[index] = tmp[part2]
+            part2 += 1
+        index += 1
+
+    for i in range(0, mid - part1 + 1):
+        arr[index + i] = tmp[part1 + i]
+
+
+def _mergeSort(arr, tmp, start, end):
+    if start < end:
+        mid = (start + end) // 2
+        _mergeSort(arr, tmp, start, mid)
+        _mergeSort(arr, tmp, mid + 1, end)
+        merge(arr, tmp, start, mid, end)
+
+
+def mergeSort(arr):
+    tmp = [0] * len(nums)  # 들어온 배열의 크기만큼 임시 저장 공간
+    _mergeSort(arr, tmp, 0, len(arr) - 1)  # 정렬할 타겟 배열, 임시 배열, 시작점, 끝점을 인자로 넘겨주는 함수
+
+
+mergeSort(nums)
+for elem in nums:
+    print(elem)
--- changsub/9663.py
+++ changsub/9663.py
@@ -1,22 +1,43 @@
-N = int(input())
+# # M : 현재 놓인 퀸의 갯수, N과 동일한 경우 모든 퀸이 체스판이 놓인 상태
+# # row : 현재 놓인 퀸의 row 위치
+# # col : 현재 놓인 퀸의 col 위치
+# def reculsive(M, row, col):
+#     if M == N:
+#         # 조건이 완성 된 상태
+#         return
+#     for i in range(N):
+#         if col != i:
+#             reculsive(M + 1, row + 1, i)
+#         check[row + 1][i] = 0
+#
+#
+# for row in range(N):
+#     reculsive(1, row, 0)
 
-check = [[0] * N for _ in range(N)]
+n = int(input())
 
-print(check)
+cnt = 0
+column = [0] * n
 
 
-# M : 현재 놓인 퀸의 갯수, N과 동일한 경우 모든 퀸이 체스판이 놓인 상태
-# row : 현재 놓인 퀸의 row 위치
-# col : 현재 놓인 퀸의 col 위치
-def reculsive(M, row, col):
-    if M == N:
-        # 조건이 완성 된 상태
+def validation(x):
+    for i in range(x):
+        if column[x] == column[i] or abs(column[x] - column[i]) == abs(x - i):
+            return False
+    return True
+
+
+def n_queens(x):
+    global cnt
+    if x == n:
+        cnt += 1
         return
-    for i in range(N):
-        if col != i:
-            reculsive(M + 1, row + 1, i)
-        check[row + 1][i] = 0
+    else:
+        for i in range(n):
+            column[x] = i
+            if validation(x):
+                n_queens(x + 1)
 
 
-for row in range(N):
-    reculsive(1, row, 0)
+n_queens(0)
+print(cnt)
