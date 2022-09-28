2c5f2033b591708f4cefe2d8c302606bc815d4c3
hcs4125 <hcs4125@gmail.com>
2022-09-28 21:04:32 +0900

    update:11729

---
+++ changsub/11729.py
@@ -0,0 +1,14 @@
+
+# N - 1개의 원판은 처음 1 -> 3 -> 2로 옮겨졌다가, 2 -> 1 -> 3으로 최종적으로 옮겨진다.
+def hanoi(num, start, to, end):
+    if num == 1:
+        print(start, end)
+    else:
+        hanoi(num - 1, start, end, to)
+        print(start, end) # 가장 큰 원판이 1 -> 3으로 옮겨진 것을 출력
+        hanoi(num - 1, to, end, start)
+
+
+N = int(input())
+print(2 ** N - 1)
+hanoi(N, 1, 2, 3)
