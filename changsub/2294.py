ea0afc101a6a7050a5b6bb623e194de523eb75e8
brido4125 <hcs4125@gmail.com>
2022-10-12 13:32:53 +0900

    solved:동전2 - retry

--- changsub/2294.py
+++ changsub/2294.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 
 N, target = map(int, input().split())
 
@@ -6,5 +7,41 @@ coins = []
 for _ in range(N):
     coins.append(int(sys.stdin.readline()))
 
-sum = 0
+# 코인 중복 제거
+coins = set(coins)
+
+visited = [False] * (target + 1)
+
+depth = 1 # 사용한 동전 개수
+
+
+def bfs():
+    queue = deque()
+    find = False
+    for coin in coins:
+        if coin < target:
+            # 주어진 동전 각 1개씩 사용하는 경우 => Depth 1
+            queue.append([coin, 1])
+            visited[coin] = 1
+
+    while queue:
+        pop, depth = queue.popleft()
+        if target == pop:
+            print(depth)
+            find = True
+            break
+        for coin in coins:
+            next = pop + coin
+            next_depth = depth + 1
+            if next > target:
+                continue
+            elif next <= target and visited[next] == 0:
+                visited[next] = True
+                queue.append([next, next_depth])
+
+    if not find:
+        print('-1')
+
+
+bfs()
 
