99946b5bde5a0dc87547ebdda02481457ccf308d
brido4125 <hcs4125@gmail.com>
2022-10-12 14:25:46 +0900

    resolved:줄 세우기=

--- changsub/2252.py
+++ changsub/2252.py
@@ -1,27 +1,31 @@
 import sys
+from collections import deque
 
 N, M = map(int, input().split())
 
 adj_list = [[] for _ in range(N + 1)]
-income = [0] * (N + 1)
+indegree = [0] * (N + 1)
 answers = []
 visited = [False] * (N + 1)
 
 for _ in range(M):
     start, end = map(int, sys.stdin.readline().split())
     adj_list[start].append(end)
-    income[end] += 1
+    indegree[end] += 1
 
-while len(answers) != N:
-    for i in range(1, N + 1):
-        if visited[i]:
-            continue
-        elif income[i] == 0:
-            answers.append(i)
-            visited[i] = True
-            for elem in adj_list[i]:
-                income[elem] -= 1
-            adj_list[i].clear()
+queue = deque()
+
+for i in range(1,N+1):
+    if indegree[i] == 0:
+        queue.append(i)
+
+while queue:
+    popleft = queue.popleft()
+    answers.append(popleft)
+    for elem in adj_list[popleft]:
+        indegree[elem] -= 1
+        if indegree[elem] == 0:
+            queue.append(elem)
 
 for target in answers:
     print(target,end=" ")
