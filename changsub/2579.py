b693bac6a245614721dd48b40f67eb97181e34a6
brido4125 <hcs4125@gmail.com>
2022-10-15 11:00:49 +0900

    solve:계단 오르기

--- changsub/2579.py
+++ changsub/2579.py
@@ -1,13 +1,35 @@
 import sys
 
 N = int(input())
-stairs = []
+stairs = [0]
 for _ in range(N):
     stairs.append(int(sys.stdin.readline()))
 
-memoization = []
+memoization = [0] * (N + 1)
 
-# bottom-up 방식이 적합
+memoization[0] = stairs[0]  # 0번째 비우고 시작
+memoization[1] = stairs[1]
+
+dp = [0] * (N + 1)
+
+dp[0] = stairs[0]  # 0번째 비우고 시작
+dp[1] = stairs[1]
+
+# Top-Down 방식
+def reculsive(N):
+    if N <= 0:
+        return 0
+    if memoization[N] == 0:
+        memoization[N] = max(reculsive(N - 2), reculsive(N - 3) + stairs[N - 1]) + stairs[N]
+    return memoization[N]
 
 
-print(memoization)
+if N >= 2:
+    memoization[2] = stairs[2] + stairs[1]
+    dp[2] = stairs[2] + stairs[1]
+
+#Bottom-Up 방식
+for i in range(3, N + 1):
+    dp[i] = stairs[i] + max(dp[i - 3] + stairs[i - 1], dp[i - 2])
+print(reculsive(N))
+print(dp[N])
