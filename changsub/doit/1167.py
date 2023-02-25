488bc57e4cfff84ed01e0bcdd20fd0820d9b692b
brido4125 <hcs4125@gmail.com>
2023-02-25 16:13:22 +0900

    update:트리의 지름

--- changsub/doit/1167.py
+++ changsub/doit/1167.py
@@ -1,10 +1,40 @@
 import sys
+from collections import deque
 
 N = int(sys.stdin.readline())
 
-adj_list = []
+# 깊은 복사를 위해 for문으로 배열 생성
+adj_list = [[] for _ in range(N + 1)]
 
+# input 정보를 adj_list 형태로
 for i in range(N):
-    adj_list.append(list(map(int, sys.stdin.readline().split())))
+    target = list(map(int, sys.stdin.readline().split()))
+    for j in range(1, len(target) - 1, 2):
+        adj_list[target[0]].append((target[j], target[j + 1]))
 
-print(adj_list)
+distance = [0 for _ in range(N + 1)]
+visited = [False for _ in range(N + 1)]
+
+
+def bfs(start):
+    visited[start] = True
+    my_queue = deque([start])
+    while my_queue:
+        target = my_queue.popleft()
+        for elem in adj_list[target]:
+            if not visited[elem[0]]:
+                distance[elem[0]] += (distance[target] + elem[1])
+                my_queue.append(elem[0])
+                visited[elem[0]] = True
+
+
+# 임의의 한점에서 가장 멀리 떨어져 있는 한 점 찾기
+bfs(2)
+far_node = distance.index(max(distance))#임의의 한점으로부터 가장 멀리 떨어진 점
+# visited, distance 리스트 초기화
+for i in range(N+1):
+    distance[i] = 0
+    visited[i] = False
+bfs(far_node)
+answer = max(distance)
+print(answer)
